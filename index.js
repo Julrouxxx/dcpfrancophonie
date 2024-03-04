89291611c77426fe517fb36b8d87021c5b9a426b
Julien <julien.labbe@moovingdev.fr>
2024-03-05 00:06:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const { Client, GatewayIntentBits, Events, ActivityType } = require('discord.js');
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3055
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.Guilds
+	]
+});
+let list1 = [];
+let list2 = [];
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+    c.user.setPresence({
+        activities: [{name: " tes conversations", type: ActivityType.Watching}]
+    })
+});
+
+client.on(Events.MessageCreate, m => {
+    if(m.author.id === client.user.id) return;
+    let message = " " + m.content.toUpperCase() + " ";
+
+    if(list2.some(v => message.includes(" " + v.trigger + " "))){
+        const v = list2.find(v => message.includes(" " + v.trigger + " "));
+        m.reply(`Attention au nom ! Tu ne diras pas **${v.trigger.toLowerCase()}**, tu diras **${v.replace}**. ☝️🫠`)
+        return;    
+    }
+    if(list1.some(v => message.includes(" " + v.trigger + " "))){
+        const v = list1.find(v => message.includes(" " + v.trigger + " "));
+        m.reply(`Attention aux anglicismes ! Tu ne diras pas **${v.trigger.toLowerCase()}**, tu diras **${v.replace}**. ☝️🤓`)
+        return;
+    }});
+
+try{
+    let data = fs.readFileSync('liste1.json');
+    let parsedData = JSON.parse(data);
+    parsedData = parsedData.map(element => {
+        return {trigger: element.trigger.toUpperCase(), replace: element.replace}
+    });
+    list1.push(...parsedData);
+    data = fs.readFileSync('liste2.json');
+    parsedData = JSON.parse(data);
+    parsedData = parsedData.map(element => {
+        return {trigger: element.trigger.toUpperCase(), replace: element.replace}
+    });
+    list2.push(...parsedData);
+    console.log("Data loaded");
+}catch(e){
+        console.log('No data found', e);
+}
+
+
+client.login("MTIwOTY2Nzc1MDkxMDIzNDYyNg.GcJZe9.tCaZ0QAxn_GSKvi4sUEbnIRaIsdhrBnzUyYvrI");
+
+app.use(bodyParser.json());
+
+app.get("/", (_, res)=> {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get("/list1", (_, res)=> {
+    res.send(list1)
+})
+
+app.get("/list2", (_, res)=> {
+    res.send(list2)
+})
+
+app.post("/list1", (req, res)=>{
+    console.log(req.body)
+    const parsedData = (req.body).map(element => {
+        return {trigger: element.trigger.toUpperCase(), replace: element.replace}
+    });
+    list1 = parsedData;
+    console.log('Saving data list2');
+	const data = JSON.stringify(list1);
+	fs.writeFileSync('liste1.json', data);
+    res.send("OK")
+})
+
+app.post("/list2", (req, res)=>{
+
+    const parsedData = (req.body).map(element => {
+        return {trigger: element.trigger.toUpperCase(), replace: element.replace}
+    });
+    list2 = parsedData;
+    console.log('Saving data list2');
+	const data = JSON.stringify(list2);
+	fs.writeFileSync('liste2.json', data);
+    res.send("OK")
+})
+
+app.listen(port, () => {
+    console.log("Serveur web lancé sur ", port);
+})
